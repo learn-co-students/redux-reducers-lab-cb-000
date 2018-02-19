2cd5e6d6e6edd743d33215d49c2b258ca99cf581
tahbristol <tahbristol@gmail.com>
2018-02-19 13:41:14 -0500

    Done.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,21 @@
-export default function ingredientReducer() {
+export default function ingredientReducer(state = [], action) {
+	switch (action.type) {
+		case 'ADD_INGREDIENT':
+			return [
+				...state,
+					action.payload
+				]
+		case 'REMOVE_INGREDIENT':
+		let newState = [];
+			state.map((ingredient) => {
+				if(ingredient !== action.payload){
+					newState.push(ingredient);
+				}
+			})
+			return newState;
+		default:
+			return state;
+	}
+
 
 }
