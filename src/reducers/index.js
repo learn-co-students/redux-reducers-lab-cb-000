923df3d17af6ebb7fbe3b81cdc69ef4ed79eb506
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-06-08 21:43:07 +0000

    Done.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,21 @@
-export default function ingredientReducer() {
+import Ingredient from '../components/ingredient'
+import {addIngredient} from '../actions/index'
+import {removeIngredient} from '../actions/index'
 
+export default function ingredientReducer(state=[], action) {
+  switch(action.type){
+    case 'ADD_INGREDIENT':
+      return [
+        ...state,
+        action.payload
+      ]
+
+    case 'REMOVE_INGREDIENT':
+      return state.filter(ingredient =>
+          ingredient !== action.payload
+      )
+
+    default:
+      return state
+    }
 }
